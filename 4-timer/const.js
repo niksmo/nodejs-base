712ee49277f42a1da4eb8017857d61b9f3b400a3
nikolay <n.smolov@yandex.ru>
2023-07-19 19:18:03 +0300

    feat(4-timer): refactor after code review

--- 4-timer/const.js
+++ 4-timer/const.js
@@ -1,9 +1,15 @@
+export const TYPE_IN_MS = {
+  h: 1000 * 60 * 60,
+  m: 1000 * 60,
+  s: 1000,
+};
+
 export const START = 'start';
+
 export const END = 'end';
+
 export const ERROR = 'error';
 
-export const TIME = {
-  HOUR: 'h',
-  MIN: 'm',
-  SEC: 's',
-};
+export const ERR_MSG = `Передайте параметры в формате: 0h 0m 12s
+Важно указывать тип передаваемой единицы: h, m или s
+Программа принимает от 1 до 3 параметров, последовательность не имеет значение`;
